abb36a1f95a91a2f3e6b3a4dbe4e30640a0f3a5a
Gopa Sen <gopasen@yahoo.com>
2015-02-19 01:53:37 +0800

    Second version with modification in file comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## The functiona below cab be used to compute, cache and return the  inverse of a matrix. If the inverse of a matrix has already been computed earlier, then it retrieves the inverse from the cache.
+## The functiona below can be used to compute, cache and retrieve  inverse of a matrix. If the inverse of a matrix has already been computed earlier, then it retrieves the inverse from the cache.
 
 
 
